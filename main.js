fdfccd49db66a09a02f4c872050662f16eaab313
Brian Meinert <brian.meinert1@gmail.com>
2025-06-16 17:41:56 -0400

    add event listener to select the rating element. Update submit button to only function with a selected rating

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const submitButton = document.querySelector('.js-submit-btn');
 const thankYouElement = document.querySelector('.js-thank-you-content');
 const cardElement = document.querySelector('.js-card-content');
+const ratingElements = document.querySelectorAll('.js-rating-button');
+const ratingValue = document.querySelector('.review-value');
 
+// Event Listener for rating buttons
+ratingElements.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    ratingElements.forEach((btn) => btn.classList.remove('active'));
+    event.target.classList.add('active');
+    return;
+  });
+});
+
+// Submit button event listener
 submitButton.addEventListener('click', () => {
-  cardElement.classList.add('hidden');
-  thankYouElement.classList.remove('hidden');
+  // Check if a rating is selected to proceed to thank you page
+  const selectedRating = document.querySelector('.js-rating-button.active');
+  if (!selectedRating) {
+    return;
+  } else {
+    cardElement.classList.add('hidden');
+    thankYouElement.classList.remove('hidden');
+    ratingValue.textContent = selectedRating.getAttribute('value');
+  }
 });
